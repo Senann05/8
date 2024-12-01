452926d8555a13bcf43748d44e4e674f5eb2e03d
Senann05 <senaneminli000@gmail.com>
2024-12-01 23:21:40 +0400

    4last

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const App = () => {
+  const [buttonText, setButtonText] = useState("Button 1");
+
   return (
-    <div
-    >
-      <button className="button">Button 1</button>
-      <button className="button" onClick={() => alert("Warning: Button 2 activated!")}>Button 2</button>
-      <button className="button" title="Button 3 is special">Button 3</button>
+<div>
+      <button className="button" onClick={() => setButtonText("Hello")}>{buttonText}</button>
+      <button className="button2" onClick={() => {alert("Warning: Button 2 activated!")}}>Button 2</button>
+      <button className="button" title="Button 3">Button 3</button>
 
     </div>
   );
